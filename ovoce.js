e9df7bdd91fed1a22f84aeb13997eed8d47ff681
Veronika Bosáková <veronikabosakova@gmail.com>
2022-04-05 18:33:25 +0200

    [REFACTOR] Change querySelector to getElementById

--- ovoce.js
+++ ovoce.js
@@ -1,5 +1,5 @@
-let prvniOvoce = document.querySelector("#muj-div").dataset.ovoce;
-let druheOvoce = document.querySelector("#muj-druhy-div").dataset.ovoce;
+let prvniOvoce = document.getElementById("muj-div").dataset.ovoce;
+let druheOvoce = document.getElementById("muj-druhy-div").dataset.ovoce;
 console.log(prvniOvoce);
 console.log(druheOvoce);
 
