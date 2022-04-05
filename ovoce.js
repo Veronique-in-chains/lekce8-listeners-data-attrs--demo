4a6643b13df8936adfd4c22c07ee340c8d67eb55
Veronika Bosáková <veronikabosakova@gmail.com>
2022-04-05 18:28:36 +0200

    Obtain value from data attributes

--- ovoce.js
+++ ovoce.js
@@ -1,1 +1,4 @@
-console.log("test");
+let prvniOvoce = document.querySelector("#muj-div").dataset.ovoce;
+let druheOvoce = document.querySelector("#muj-druhy-div").dataset.ovoce;
+console.log(prvniOvoce);
+console.log(druheOvoce);
